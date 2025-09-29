e172aaf300e2d25d956599ff60d584e97926bfd5
Sergio Ventura <sergiovforesto@gmail.com>
2025-09-28 21:49:37 -0400

    autoplay video - playInline loop

--- src/presentations/components/landings/video/video-mux-player.tsx
+++ src/presentations/components/landings/video/video-mux-player.tsx
@@ -3,31 +3,14 @@ import { useState, useEffect } from "react";
 import MuxPlayer from "@mux/mux-player-react";
 
 export default function VideoMuxPlayer() {
-  const [canAutoplay, setCanAutoplay] = useState(false);
-
-  useEffect(() => {
-    // Esperar a que el usuario interactúe con la página
-    const handleUserInteraction = () => {
-      setCanAutoplay(true);
-      document.removeEventListener("click", handleUserInteraction);
-      document.removeEventListener("scroll", handleUserInteraction);
-    };
-
-    document.addEventListener("click", handleUserInteraction);
-    document.addEventListener("scroll", handleUserInteraction);
-
-    return () => {
-      document.removeEventListener("click", handleUserInteraction);
-      document.removeEventListener("scroll", handleUserInteraction);
-    };
-  }, []);
-
   return (
     <MuxPlayer
       playbackId="oE0102pmWDq762eVFfw68tfmr7jZjahfapv7IL1KN3HL4"
       accentColor="#ea580c"
-      playsInline={true}
-      autoPlay={canAutoplay ? "true" : false}
+      playsInline
+      autoPlay
+      muted
+      loop
       metadata={{
         videoTitle: "Test VOD",
         ViewerUserId: "user-id-007",
